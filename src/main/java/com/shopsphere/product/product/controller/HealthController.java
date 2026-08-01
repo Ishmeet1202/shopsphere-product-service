57a3b4abd3020a0c5151ba42762a10edfc2fd7a9
Ishmeet Singh <ishmeet.singh1202@gmail.com>
2026-08-02 01:39:10 +0530

    refactor: health check endpoint refactored

--- src/main/java/com/shopsphere/product/product/controller/HealthController.java
+++ src/main/java/com/shopsphere/product/product/controller/HealthController.java
@@ -1,15 +1,15 @@
 package com.shopsphere.product.product.controller;
 
+import com.shopsphere.product.product.dto.response.HealthResponseDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/health")
+@RequestMapping("/api/v1")
 public class HealthController {
 
     @Value("${spring.application.name}")
@@ -18,14 +18,14 @@ public class HealthController {
     @Value("${spring.application.version}")
     private String version;
 
-    @GetMapping()
-    public ResponseEntity<Map<String,String>> healthCheck() {
+    @GetMapping("/health")
+    public ResponseEntity<HealthResponseDto> getHealth() {
         return ResponseEntity.ok(
-                Map.of(
-                        "service",serviceName,
-                        "status", "UP",
-                        "version", version
-                )
+                HealthResponseDto.builder()
+                        .serviceName(serviceName)
+                        .status("UP")
+                        .version(version)
+                        .build()
         );
     }
 }
