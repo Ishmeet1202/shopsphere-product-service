6eeb0cd11664330d26f9019fb873e23e4ad1291b
Ishmeet Singh <ishmeet.singh1202@gmail.com>
2026-08-02 17:38:35 +0530

    refactor: add a hepler method to build error response

--- src/main/java/com/shopsphere/product/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shopsphere/product/exception/GlobalExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.shopsphere.product.exception;
 
 import com.shopsphere.product.product.dto.response.ErrorResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -15,45 +15,61 @@ import java.util.List;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private final String VALIDATION_ERROR = "VALIDATION_ERROR";
-    private final String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
+    private final static String VALIDATION_ERROR = "VALIDATION_ERROR";
+    private final static String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
-            WebRequest request) {
+            HttpServletRequest request) {
 
         List<String> errors = new ArrayList<>();
 
         ex.getFieldErrors().forEach(error -> errors.add(error.getDefaultMessage()));
 
-        ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
-                .timestamp(LocalDateTime.now())
-                .status(HttpStatus.BAD_REQUEST.toString())
-                .error(VALIDATION_ERROR)
-                .message("Request validation failed.")
-                .validationErrors(errors)
-                .path(request.getDescription(false))
-                .build();
-
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(errorResponseDto);
+                .body(buildErrorResponse(
+                        LocalDateTime.now(),
+                        HttpStatus.BAD_GATEWAY.value(),
+                        VALIDATION_ERROR,
+                        "Request Validation failed",
+                        errors,
+                        request
+                        )
+                );
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleAllOther(
             Exception ex,
-            WebRequest request
+            HttpServletRequest request
     ) {
-        ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
-                .timestamp(LocalDateTime.now())
-                .status(HttpStatus.INTERNAL_SERVER_ERROR.toString())
-                .error(INTERNAL_SERVER_ERROR)
-                .message("Something went wrong")
-                .path(request.getDescription(false))
-                .build();
-
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(errorResponseDto);
+                .body(buildErrorResponse(
+                        LocalDateTime.now(),
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        INTERNAL_SERVER_ERROR,
+                        "An unexpected error occurred.",
+                        null,
+                        request
+                ));
+    }
+
+    private ErrorResponseDto buildErrorResponse(
+            LocalDateTime timestamp,
+            int status,
+            String error,
+            String message,
+            List<String> validationErrors,
+            HttpServletRequest request
+    ) {
+        return ErrorResponseDto.builder()
+                .timestamp(timestamp)
+                .status(status)
+                .error(error)
+                .message(message)
+                .validationErrors(validationErrors)
+                .path(request.getRequestURI())
+                .build();
     }
 }
